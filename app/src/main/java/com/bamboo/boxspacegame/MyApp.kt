2cf0bcc34b8c054813938621d12f9dfe11bc418e
greentea107 <52137124@qq.com>
2021-03-12 14:12:44 +0800

    代码优化

--- app/src/main/java/com/bamboo/boxspacegame/MyApp.kt
+++ app/src/main/java/com/bamboo/boxspacegame/MyApp.kt
@@ -10,6 +10,8 @@ import android.view.View
 import android.view.WindowManager
 import androidx.core.content.edit
 import com.jeremyliao.liveeventbus.LiveEventBus
+import com.pgyer.pgyersdk.PgyerSDKManager
+import com.pgyer.pgyersdk.pgyerenum.FeatureEnum
 import com.tencent.bugly.crashreport.CrashReport
 
 class MyApp : Application() {
@@ -53,7 +55,14 @@ class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
         context = applicationContext
+
         CrashReport.initCrashReport(applicationContext, "135384028a", false)
+
+        PgyerSDKManager.InitSdk()
+            .setContext(context)
+            .enable(FeatureEnum.CHECK_UPDATE)  //添加检查新版本
+            .build()
+
         LiveEventBus.config().enableLogger(false).lifecycleObserverAlwaysActive(false)
         registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
             override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
